d30948f086c05b592fa35dc7d1085ec2cb0d60d1
Minebox260 <39033036+Minebox260@users.noreply.github.com>
2023-12-20 18:12:09 +0100

    print fix

--- server_comm.c
+++ server_comm.c
@@ -54,12 +54,15 @@ void * handle_request(void * arg) {
     int i;
     int id_ressource;
     request_t_data * data = (request_t_data *) arg;
-    ptr = strtok(data->request, ":");
-    printf("Received request: %s", data->request);
+
+    printf("Received request from %s: %s\n", ip_client, data->request);
+    
     // Récupération de l'adresse IP + port de la voiture émittrice de la requête
     ip_client[0] = '\0';
     sprintf(ip_client, "%s:%d", inet_ntoa(data->p_adr_client->sin_addr), ntohs(data->p_adr_client->sin_port));
   
+    ptr = strtok(data->request, ":");
+
     // Analyse du code de requête
     code = atoi(ptr);
     
